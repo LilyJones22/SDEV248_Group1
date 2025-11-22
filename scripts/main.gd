b54e76e7e7c85d790c70e2c5de1452baec3370f3
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-11-21 23:43:15 -0500

    Add files via upload

---
+++ scripts/main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var win_scene: PackedScene
+@export var lose_scene: PackedScene
+
+# remove notes and add these to necessary places - Lily
+# if (insert win condition):
+#	get_tree().change_scene_to_packed(win_scene)
+# if (insert lose condition):
+#	get_tree().change_scene_to_packed(lose_scene)
---
+++ scripts/player_character.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@onready var _player_sprite = $PlayerSprite
+
+var health = 3
+const SPEED = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+		_player_sprite.play("walk")
+	else:
+		_player_sprite.stop()
+
+	move_and_slide()
